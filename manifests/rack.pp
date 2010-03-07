fb583d577db7976eb63241c695d74fb8343e0c06
root <root@collector.(none)>
2010-03-07 00:55:39 -0600

    moved to puppet::server::rack

--- manifests/rack.pp
+++
@@ -1,9 +0,0 @@
-class puppet::rack {
-File{mode=>'0755', owner=>'root', group=>'root'}
-file { ["/etc/puppet/rack", "/etc/puppet/rack/public"]:
-    ensure => directory,
-  }
-  file { "/etc/puppet/rack/config.ru":
-    source => "puppet:///modules/puppet/config.ru",
-  } 
-}
