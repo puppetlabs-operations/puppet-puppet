f9c0ec599e5c2c7879668a5898be9c054624756d
Dan Bode <dan@bodepd.com>
2010-03-17 22:03:00 -0500

    added support for puppet with rack and passenger

--- manifests/server/passenger.pp
+++ manifests/server/passenger.pp
@@ -1,12 +1,19 @@
-class puppet::server::passenger {
+class puppet::server::passenger inherits puppet::server {
   include ::passenger
   require apache::ssl
   include puppet::server::rack
+  include ::passenger::params
+  $passenger_version=$::passenger::params::version
+  Service['puppetmaster']{
+    enable => false,
+    ensure => stopped,
+  }
   apache::vhost{'puppetmaster':
     port => '8140',
     docroot => '/etc/puppet/rack/public/',
     webdir => '/etc/puppet/rack/',
     ssl => true,
     template => 'puppet/apache2.conf.erb',
+    require => Service['puppetmaster'],
   }
 }
