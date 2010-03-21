d4ca491d854b94dee7dea22b35ecdb045c0649d2
root <root@baal.puppetlabs.com>
2010-03-21 17:29:01 +0000

    fixed puppetd startup and account password setting

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,9 +18,21 @@ class puppet {
   package{'puppet':
     ensure => installed,
   }
+  #
+  # This a hack for debian because the init script checks
+  # /etc/defaults/puppet for $START. Not sure how we want to approach fixing this.
+  # Can't be modeled with params.  Redhat has a default directory but no puppet file.
+  #
+  file {'/etc/default/puppet':
+    mode => '0644',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///modules/puppet/etc_default_puppet',
+  }
   service{ $puppet::params::puppetd_service:
-    ensure    => running,
-    enable    => true,
+    ensure => running,
+    enable => true,
     hasstatus => true,
+    require => File['/etc/default/puppet']
   }
 }
--- manifests/server.pp
+++ manifests/server.pp
@@ -13,15 +13,19 @@
 class puppet::server {
   include puppet
   include puppet::passenger
-
   package { $puppet::params::puppetmaster_package:
     ensure => present,
   }
-  
   file{'/etc/puppet/namespaceauth.conf':
-    owner  => root,
-    group  => root,
-    mode   => 644,
+    owner => root,
+    group => root,
+    mode => 644,
     source => 'puppet:///modules/puppet/namespaceauth.conf',
+  }
+  service {'puppetmaster': 
+    ensure => stopped, 
+    enable => false, 
+    hasstatus => true,
+    before => Service['httpd'] 
   }
 }
