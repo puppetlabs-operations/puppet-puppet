9531ebc502e177ccebfb85b5d86f8b74d6f0bbc0
Zach Leslie <zach@puppetlabs.com>
2011-06-29 14:46:41 -0700

    fix paramater name

--- manifests/server/backup.pp
+++ manifests/server/backup.pp
@@ -1,8 +1,8 @@
 class puppet::server::backup {
   bacula::job {
     "${fqdn}-puppetmaster":
-      files   => ["/etc/puppet","/var/lib/puppet"],
-      exclude => ["/var/lib/puppet/reports"]
+      files    => ["/etc/puppet","/var/lib/puppet"],
+      excludes => ["/var/lib/puppet/reports"]
   }
 }
 
