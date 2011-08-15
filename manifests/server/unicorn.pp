06348667e55c74d240a7a3c5a1f179c2477aced7
Zach Leslie <zach@puppetlabs.com>
2011-08-15 16:08:03 -0700

    add unicorn server method

---
+++ manifests/server/unicorn.pp
@@ -0,0 +1,27 @@
+class puppet::server::unicorn {
+
+  unicorn::app {
+    "puppetmaster":
+      approot    => "/etc/puppet",
+      config     => "/etc/puppet/unicorn.conf",
+      require    => File["/etc/puppet/unicorn.conf"],
+      initscript => "puppet/unicorn_puppetmaster",
+  }
+
+  file {
+    "/etc/puppet/unicorn.conf":
+      owner  => root,
+      group  => root,
+      mode   => 644,
+      source => "puppet:///modules/puppet/unicorn.conf";
+  }
+
+  file {
+    "/etc/puppet/config.ru":
+      owner  => root,
+      group  => root,
+      mode   => 644,
+      source => "puppet:///modules/puppet/config.ru";
+  }
+
+}
