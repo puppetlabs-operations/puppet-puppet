3d06606f736cbe956293e9ba71c4b9c2543a74e4
Zach Leslie <zach@puppetlabs.com>
2011-08-18 10:43:06 -0700

    disable hasstatus for testing on debian

--- manifests/server.pp
+++ manifests/server.pp
@@ -89,7 +89,7 @@ class puppet::server (
     service {'puppetmaster':
       ensure    => stopped,
       enable    => false,
-      hasstatus => true,
+      #zleslie hasstatus => true, # this is broken on debian if its disabled, since 0 is still returned when not running
       require   => File['/etc/puppet/puppet.conf'];
     }
   }
