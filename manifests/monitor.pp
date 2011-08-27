db9c98e3ff133744a0c3e100fd8d63d52266f2c6
Zach Leslie <zach@puppetlabs.com>
2011-08-26 23:04:58 -0700

    attempt to monitor only on hosts that are running nagios

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -8,22 +8,24 @@ class puppet::monitor (
     $ensure = "absent"
   }
 
-  include nagios::params
+  if defined(Class["nagios"]) {
+    include nagios::params
 
-  @@nagios_service { "check_puppetd_${hostname}":
-    ensure    => $ensure,
-    use       => 'generic-service',
-    host_name => "$fqdn",
-    check_command => $puppetversion ? {
-      '0.25.4' => 'check_nrpe!check_proc!1:1 puppetd',
-      default  => $operatingsystem ? {
-        CentOS  => 'check_nrpe!check_proc!1:1 puppetd',
-        default => 'check_nrpe!check_proc!1:1 puppet',
+    @@nagios_service { "check_puppetd_${hostname}":
+      ensure    => $ensure,
+      use       => 'generic-service',
+      host_name => "$fqdn",
+      check_command => $puppetversion ? {
+        '0.25.4' => 'check_nrpe!check_proc!1:1 puppetd',
+        default  => $operatingsystem ? {
+          CentOS  => 'check_nrpe!check_proc!1:1 puppetd',
+          default => 'check_nrpe!check_proc!1:1 puppet',
+        },
       },
-    },
-    service_description => "check_puppetd_${hostname}",
-    target              => '/etc/nagios3/conf.d/nagios_service.cfg',
-    notify              => Service[$::nagios::params::nagios_service],
+      service_description => "check_puppetd_${hostname}",
+      target              => '/etc/nagios3/conf.d/nagios_service.cfg',
+      notify              => Service[$::nagios::params::nagios_service],
+    }
   }
 
 }
