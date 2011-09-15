39285ebab11a48294bf936a866c2a61e2afdbe92
Zach Leslie <zach@puppetlabs.com>
2011-09-15 16:03:25 -0700

    remove site.pp since we are using the one stored in the repo now

--- manifests/server.pp
+++ manifests/server.pp
@@ -47,10 +47,9 @@ class puppet::server (
     $servertype   = "passenger"
   ){
 
-    #  file { "/etc/puppet/manifests/site.pp":
-    #    #content => 'node default { include puppetlabs::base }'
-    #    content => 'import "/etc/puppet/environments/$environment/nodes/*.pp"'
-    #  }
+    file { "/etc/puppet/manifests/site.pp":
+      ensure      => absent,
+    }
 
   case $servertype {
     "passenger": {
