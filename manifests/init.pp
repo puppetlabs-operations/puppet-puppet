c2b2db5f1ef3f53d0a8417b0d05456894498b725
Zach Leslie <zach@puppetlabs.com>
2011-09-30 10:00:09 -0700

    test better logic for making puppet upgrades

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,7 +29,7 @@ class puppet (
   $puppet_vardir   = $puppet::params::puppet_vardir
   $puppet_ssldir   = $puppet::params::puppet_ssldir
 
-  if ! $kernel == "Darwin" {
+  if $kernel != "Darwin" {
     package { 'puppet':
       ensure => latest,
     }
