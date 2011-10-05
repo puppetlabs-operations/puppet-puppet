21b1003e6c1ed2f56cc759ec4542100c3f55b8e4
Ben Hughes <ben@puppetlabs.com>
2011-10-05 16:06:15 -0700

    Can't use 'latest' on FreeBSD package provider

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,7 +29,7 @@ class puppet (
   $puppet_vardir   = $puppet::params::puppet_vardir
   $puppet_ssldir   = $puppet::params::puppet_ssldir
 
-  if $kernel != "Darwin" {
+  if $kernel != "Darwin" and $kernel != "FreeBSDa "{
     package { 'puppet':
       ensure => latest,
     }
