fb225e72332bf934ba0f6e924d8b97cb8f723c74
Zach Leslie <zach@puppetlabs.com>
2012-05-18 09:45:41 -0700

    add hack for lsbrelease in version 1.6.9

--- manifests/hack.pp
+++ manifests/hack.pp
@@ -9,6 +9,12 @@ class puppet::hack {
         source => 'puppet:///modules/puppet/virtual.rb'
       }
     }
+    if $::facterversion == '1.6.9' {
+      # Patch facter.
+      file{ '/usr/lib/ruby/1.8/facter/lsbrelease.rb':
+        source => 'puppet:///modules/puppet/lsbrelease.rb'
+      }
+    }
   }
 
 }
