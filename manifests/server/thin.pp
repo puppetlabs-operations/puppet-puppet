d585bb130aac4fa13ea9bd7163edb84359e8eb72
Adrien Thebo <adrien@puppetlabs.com>
2012-05-23 16:04:08 -0700

    Implement thin runner for puppetmaster

---
+++ manifests/server/thin.pp
@@ -0,0 +1,21 @@
+class puppet::server::thin {
+
+  include puppet::params
+
+  file { "${::puppet::params::puppet_confdir}/config.ru",
+    ensure => present,
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0644',
+    source => 'puppet:///modules/puppet/config.ru',
+    before => Thin::App['puppetmaster'],
+  }
+
+  thin::app { 'puppetmaster':
+    user   => 'puppet',
+    group  => 'puppet',
+    rackup => "${::puppet::params::puppet_confdir}/config.ru",
+  }
+
+  motd::register {"Puppet master on Thin": }
+}
