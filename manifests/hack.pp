d1bc987d73bf6a4fdb175e83d4cbde82cb35fd98
Adrien Thebo <adrien@puppetlabs.com>
2012-05-24 13:38:50 -0700

    Reorganize puppet/facter hacks/patches

--- manifests/hack.pp
+++ manifests/hack.pp
@@ -1,20 +1,18 @@
 class puppet::hack {
 
   # fix facter.
-  if $::operatingsystem == 'debian' or $::operatingsystem == 'ubuntu'
-  {
+  if $::operatingsystem == 'debian' or $::operatingsystem == 'ubuntu' {
     if $::facterversion == '1.6.6' {
       # Patch facter.
       file{ '/usr/lib/ruby/1.8/facter/virtual.rb':
-        source => 'puppet:///modules/puppet/virtual.rb'
+        source => 'puppet:///modules/puppet/patches/virtual.rb'
       }
     }
     if $::facterversion == '1.6.9' {
       # Patch facter.
       file{ '/usr/lib/ruby/1.8/facter/lsbrelease.rb':
-        source => 'puppet:///modules/puppet/lsbrelease.rb'
+        source => 'puppet:///modules/puppet/patches/lsbrelease.rb'
       }
     }
   }
-
 }
