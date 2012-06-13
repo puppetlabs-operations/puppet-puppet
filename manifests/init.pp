2c1203cce7d6a811e4eb72e1378a9728c05b7c93
Adrien Thebo <adrien@puppetlabs.com>
2012-06-13 12:15:30 -0700

    Add hiera puppet_report_server param

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,18 @@
 class puppet (
     $server        = hiera("puppet_server"),
-    $ca_server     = hiera("puppet_ca_server"),
+    $ca_server     = hiera("puppet_ca_server", hiera("puppet_server")),
+    $report_server = hiera("puppet_report_server", hiera("puppet_server")),
     $manage_agent  = false
 ) {
   include puppet::params
   include concat::setup
 
+  # FIXME this seems silly
   $puppet_server    = $server
   $puppet_ca_server = $ca_server
+  $puppet_report_server = $report_server
+
+  # REFACTOR use scope.lookupvar in templates instead of copying variables
   $agent_service    = $puppet::params::agent_service
   $puppet_conf      = $puppet::params::puppet_conf
   $puppet_logdir    = $puppet::params::puppet_logdir
