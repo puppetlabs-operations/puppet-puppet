704e9b20176b12507bf1cad95a4a5cae01daebd2
Adrien Thebo <adrien@puppetlabs.com>
2012-06-28 15:00:30 -0700

    So ARGV in config.ru doesn't get passed to the master.

--- files/config.ru/99-run.rb
+++ files/config.ru/99-run.rb
@@ -1,5 +1,5 @@
 
-ARGV << "--rack --trace"
+ARGV << "--rack"
 require 'puppet/application/master'
 # we're usually running inside a Rack::Builder.new {} block,
 # therefore we need to call run *here*.
