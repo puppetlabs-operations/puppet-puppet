9a7ff4898c4f2bf654226de27c79aecffb25fe7a
Adrien Thebo <adrien@puppetlabs.com>
2012-06-28 11:45:48 -0700

    Let unicorn puppet server use default worker count

--- manifests/server/unicorn.pp
+++ manifests/server/unicorn.pp
@@ -13,7 +13,6 @@ class puppet::server::unicorn {
       initscript               => $puppet::params::unicorn_initscript,
       unicorn_pidfile          => "${puppet::params::puppet_rundir}/puppetmaster_unicorn.pid",
       unicorn_socket           => "${puppet::params::puppet_rundir}/puppetmaster_unicorn.sock",
-      unicorn_worker_processes => '4',
       stdlog_path              => $puppet::params::puppet_logdir,
       log_stds                 => 'true',
       rack_file                => 'puppet:///modules/puppet/config.ru',
