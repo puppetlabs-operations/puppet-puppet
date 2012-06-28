663b3174294ccde3bc1fb18657b145c67060c59c
Adrien Thebo <adrien@puppetlabs.com>
2012-06-28 12:01:58 -0700

    template the templates inside templates
    WHO NEEDS CONCAT WHEN WE HAVE RUBYYYYYYYYY

--- manifests/server/unicorn.pp
+++ manifests/server/unicorn.pp
@@ -3,7 +3,7 @@ class puppet::server::unicorn {
   include puppet::params
   nginx::vhost { "puppetmaster_unicorn":
     port     => 8140,
-    template => ["puppet/vhost/nginx/unicorn.conf.erb", "puppet/vhost/nginx/base.conf.erb"],
+    template => "puppet/vhost/nginx/base.conf.erb",
   }
 
   unicorn::app {
