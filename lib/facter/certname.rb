a1c3370e0b4a9234bc712489ee4d6eb90c92176f
Adrien Thebo <adrien@puppetlabs.com>
2012-07-02 16:50:33 -0700

    Sleekify the certname fact

--- lib/facter/certname.rb
+++ lib/facter/certname.rb
@@ -1,7 +1,7 @@
 Facter.add("certname") do
-  path = '/usr/bin/puppet'
   setcode do
-    %x{#{path} --configprint certname}.chomp if File.exists?(path)
+    require 'puppet'
+    Puppet[:certname]
   end
 end
 
