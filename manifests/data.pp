d37784f76c22662b194cd93e88632c5e3baf7d47
Adrien Thebo <adrien@puppetlabs.com>
2012-08-07 12:07:31 -0700

    Add hiera support to puppet

---
+++ manifests/data.pp
@@ -0,0 +1,82 @@
+# Default puppet data
+class puppet::data {
+
+  case $operatingsystem {
+    'debian', 'ubuntu': {
+      $puppet_cmd         = '/usr/bin/puppet'
+      $agent_service      = 'puppet'
+      $agent_defaults     = '/etc/default/puppet'
+      $master_package     = 'puppetmaster'
+      $master_service     = 'puppetmaster'
+      $puppet_conf        = '/etc/puppet/puppet.conf'
+      $puppet_confdir     = '/etc/puppet'
+      $puppet_logdir      = '/var/log/puppet'
+      $puppet_vardir      = '/var/lib/puppet'
+      $puppet_ssldir      = '/var/lib/puppet/ssl'
+      $puppet_rundir      = '/var/run/puppet'
+      $unicorn_initscript = 'unicorn/initscript_newer.erb'
+    }
+    'freebsd': {
+      $puppet_cmd         = '/usr/local/bin/puppet'
+      $agent_service      = 'puppet'
+      $master_package     = ''
+      $master_service     = ''
+      $puppet_conf        = '/usr/local/etc/puppet/puppet.conf'
+      $puppet_confdir     = '/usr/local/etc/puppet'
+      $puppet_logdir      = '/var/log/puppet'
+      $puppet_vardir      = '/var/puppet'
+      $puppet_ssldir      = '/var/puppet/ssl'
+      $puppet_rundir      = '/var/run/puppet'
+      $unicorn_initscript = 'unicorn/rcscript.erb'
+    }
+    'darwin': {
+      $puppet_cmd     = '/opt/local/bin/puppet'
+      $agent_service  = 'com.puppetlabs.puppet'
+      $master_package = ''
+      $master_service = ''
+      $puppet_conf    = '/etc/puppet/puppet.conf'
+      $puppet_confdir = '/etc/puppet'
+      $puppet_logdir  = '/var/log/puppet'
+      $puppet_vardir  = '/var/lib/puppet'
+      $puppet_ssldir  = '/etc/puppet/ssl'
+      $puppet_rundir  = '/var/run'
+    }
+   'centos', 'redhat', 'fedora', 'sles': {
+      $puppet_cmd         = '/usr/bin/puppet'
+      $agent_service      = 'puppet'
+      $agent_defaults     = '/etc/sysconfig/puppet'
+      $master_package     = 'puppet-server'
+      $master_service     = 'puppetmasterd'
+      $puppet_conf        = '/etc/puppet/puppet.conf'
+      $puppet_confdir     = '/etc/puppet'
+      $puppet_logdir      = '/var/log/puppet'
+      $puppet_vardir      = '/var/lib/puppet'
+      $puppet_ssldir      = '/var/lib/puppet/ssl'
+      $puppet_rundir      = '/var/run/puppet'
+      $unicorn_initscript = 'unicorn/initscript_newer.erb'
+    }
+
+    # This stops the puppet class breaking. But really, we only have very
+    # limited support for Solaris. And only through OpenCSW
+    # Taken from: '/opt/csw/bin/puppet config print ...'
+    'solaris','sunos': {
+      $puppet_cmd         = '/opt/csw/bin/puppet'
+      $puppet_conf        = '/etc/puppet/puppet.conf'
+      $puppet_confdir     = '/etc/puppet'
+      $puppet_logdir      = '/var/log/puppet'
+      $puppet_vardir      = '/var/lib/puppet'
+      $puppet_ssldir      = '/etc/puppet/ssl'
+      $puppet_rundir      = '/var/lib/puppet/run/'
+      $agent_service      = 'svc:/network/cswpuppetd'
+    }
+  }
+
+  # Behold, the list of platforms that have horrible package mangement!
+  if $kernel == 'Darwin' or $kernel == 'SunOS' or $kernel == 'FreeBSD' or $operatingsystem == 'SLES' {
+    $update_puppet = undef
+  }
+  else {
+    $update_puppet = true
+  }
+
+}
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,83 +1,16 @@
+# Use hiera to do lookups for this module data. See data.pp for defaults.
 class puppet::params {
 
-  # ---
-  # Yup, its a params class.
-  case $operatingsystem {
-    'debian', 'ubuntu': {
-      $puppet_cmd         = '/usr/bin/puppet'
-      $agent_service      = 'puppet'
-      $agent_defaults     = '/etc/default/puppet'
-      $master_package     = 'puppetmaster'
-      $master_service     = 'puppetmaster'
-      $puppet_conf        = '/etc/puppet/puppet.conf'
-      $puppet_confdir     = '/etc/puppet'
-      $puppet_logdir      = '/var/log/puppet'
-      $puppet_vardir      = '/var/lib/puppet'
-      $puppet_ssldir      = '/var/lib/puppet/ssl'
-      $puppet_rundir      = '/var/run/puppet'
-      $unicorn_initscript = 'unicorn/initscript_newer.erb'
-    }
-    'freebsd': {
-      $puppet_cmd         = '/usr/local/bin/puppet'
-      $agent_service      = 'puppet'
-      $master_package     = ''
-      $master_service     = ''
-      $puppet_conf        = '/usr/local/etc/puppet/puppet.conf'
-      $puppet_confdir     = '/usr/local/etc/puppet'
-      $puppet_logdir      = '/var/log/puppet'
-      $puppet_vardir      = '/var/puppet'
-      $puppet_ssldir      = '/var/puppet/ssl'
-      $puppet_rundir      = '/var/run/puppet'
-      $unicorn_initscript = 'unicorn/rcscript.erb'
-    }
-    'darwin': {
-      $puppet_cmd     = '/opt/local/bin/puppet'
-      $agent_service  = 'com.puppetlabs.puppet'
-      $master_package = ''
-      $master_service = ''
-      $puppet_conf    = '/etc/puppet/puppet.conf'
-      $puppet_confdir = '/etc/puppet'
-      $puppet_logdir  = '/var/log/puppet'
-      $puppet_vardir  = '/var/lib/puppet'
-      $puppet_ssldir  = '/etc/puppet/ssl'
-      $puppet_rundir  = '/var/run'
-    }
-   'centos', 'redhat', 'fedora', 'sles': {
-      $puppet_cmd         = '/usr/bin/puppet'
-      $agent_service      = 'puppet'
-      $agent_defaults     = '/etc/sysconfig/puppet'
-      $master_package     = 'puppet-server'
-      $master_service     = 'puppetmasterd'
-      $puppet_conf        = '/etc/puppet/puppet.conf'
-      $puppet_confdir     = '/etc/puppet'
-      $puppet_logdir      = '/var/log/puppet'
-      $puppet_vardir      = '/var/lib/puppet'
-      $puppet_ssldir      = '/var/lib/puppet/ssl'
-      $puppet_rundir      = '/var/run/puppet'
-      $unicorn_initscript = 'unicorn/initscript_newer.erb'
-    }
-
-    # This stops the puppet class breaking. But really, we only have very
-    # limited support for Solaris. And only through OpenCSW
-    # Taken from: '/opt/csw/bin/puppet config print ...'
-    'solaris','sunos': {
-      $puppet_cmd         = '/opt/csw/bin/puppet'
-      $puppet_conf        = '/etc/puppet/puppet.conf'
-      $puppet_confdir     = '/etc/puppet'
-      $puppet_logdir      = '/var/log/puppet'
-      $puppet_vardir      = '/var/lib/puppet'
-      $puppet_ssldir      = '/etc/puppet/ssl'
-      $puppet_rundir      = '/var/lib/puppet/run/'
-      $agent_service      = 'svc:/network/cswpuppetd'
-    }
-  }
-
-  # Behold, the list of platforms that have horrible package mangement!
-  if $kernel == 'Darwin' or $kernel == 'SunOS' or $kernel == 'FreeBSD' or $operatingsystem == 'SLES' {
-    $update_puppet = undef
-  }
-  else {
-    $update_puppet = true
-  }
-
+  $puppet_cmd         = hiera('puppet_cmd', undef)
+  $agent_service      = hiera('agent_service', undef)
+  $agent_defaults     = hiera('agent_defaults', undef)
+  $master_package     = hiera('master_package', undef)
+  $master_service     = hiera('master_service', undef)
+  $puppet_conf        = hiera('puppet_conf', undef)
+  $puppet_confdir     = hiera('puppet_confdir', undef)
+  $puppet_logdir      = hiera('puppet_logdir', undef)
+  $puppet_vardir      = hiera('puppet_vardir', undef)
+  $puppet_ssldir      = hiera('puppet_ssldir', undef)
+  $puppet_rundir      = hiera('puppet_rundir', undef)
+  $unicorn_initscript = hiera('unicorn_initscript', undef)
 }
