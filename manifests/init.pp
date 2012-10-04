34ed0eedf9ade15aa2c502fcd60122a557a8fc10
Adrien Thebo <adrien@puppetlabs.com>
2012-10-04 10:54:35 -0700

    Add documentation for Class['puppet']

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,12 @@
+# == Class: puppet
+#
+# == Description
+#
+# This manifest provides for shared behavior and resources between the agent
+# and master.
+#
+# This module should not be directly included.
+#
 class puppet {
   include puppet::params
   include concat::setup
