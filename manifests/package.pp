ea85757ebb51a018bb871debb3e4d625bed14779
Adrien Thebo <adrien@puppetlabs.com>
2012-10-25 10:07:14 -0700

    Add ensure parameter for puppet::package
    This way the exact package version can be tuned.

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,4 +1,4 @@
-class puppet::package {
+class puppet::package($ensure = 'latest') {
 
   include puppet::params
   require puppet::package::repository
@@ -6,8 +6,8 @@ class puppet::package {
   # ----
   # Be carefull about systems that may not be able to upgrade cleanly
   if $puppet::params::update_puppet {
-    package { 'puppet': ensure => latest; }
-    package { 'facter': ensure => latest; }
+    package { 'puppet': ensure => $ensure; }
+    package { 'facter': ensure => $ensure; }
 
     # Fixes a bug. #12813
     class { '::puppet::package::patches': }
