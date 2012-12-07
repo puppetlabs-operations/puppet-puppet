59658e9bb2937788423a4abdc64a695fff183455
Joe Topjian <joe@topjian.net>
2012-12-07 21:07:59 +0000

    Added defaults for puppetdb params

--- manifests/storeconfig/puppetdb.pp
+++ manifests/storeconfig/puppetdb.pp
@@ -1,7 +1,7 @@
 # Install the puppetdb terminus. Puppetdb configuration should occur elsewhere.
 class puppet::storeconfig::puppetdb(
-  $server = hiera('puppetdb_server'),
-  $port   = hiera('puppetdb_port')
+  $server = hiera('puppetdb_server', 'localhost'),
+  $port   = hiera('puppetdb_port', '8080')
 ) {
   include puppet::params
 
