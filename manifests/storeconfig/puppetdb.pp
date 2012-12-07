7fe56fa5f5bce68e402325b3a09daee5c721362b
Joe Topjian <joe@topjian.net>
2012-12-07 23:39:47 +0000

    Removed all puppetdb config as puppetlabs-puppetdb handles it completely

--- manifests/storeconfig/puppetdb.pp
+++ manifests/storeconfig/puppetdb.pp
@@ -1,17 +1,2 @@
 # Install the puppetdb terminus. Puppetdb configuration should occur elsewhere.
-class puppet::storeconfig::puppetdb(
-  $server = hiera('puppetdb_server', 'localhost'),
-  $port   = hiera('puppetdb_port', '8080')
-) {
-  include puppet::params
-
-  file { "${::puppet::params::puppet_confdir}/puppetdb.conf":
-    ensure  => present,
-    mode    => 0644,
-    owner   => 'puppet',
-    group   => 'puppet',
-    content => template('puppet/puppetdb.conf.erb'),
-    notify => Class['puppet::server'],
-  }
-
-}
+class puppet::storeconfig::puppetdb { }
