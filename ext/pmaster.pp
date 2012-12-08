ec7260ab67ccfbd0727e49ee64216aa9df88e456
Joe Topjian <joe@topjian.net>
2012-12-08 05:20:59 +0000

    Added bootstrap manifest

---
+++ ext/pmaster.pp
@@ -0,0 +1,25 @@
+class { 'puppet::server':
+  modulepath         => '$confdir/env/$environment/modules:$confdir/modules',
+  config_version_cmd => false,
+  storeconfigs       => 'puppetdb',
+  servertype         => 'passenger',
+  monitor_server     => false,
+  backup_server      => false,
+  manifest           => '/etc/puppet/manifests/site.pp',
+}
+class { 'puppet::agent':
+  server        => 'puppet.example.com',
+  report_server => 'puppet.example.com',
+  ca_server     => 'puppet.example.com',
+  method        => 'service',
+  custom_repo   => false,
+  environment   => 'development',
+}
+class { 'puppetdb':
+  database           => 'embedded',
+  listen_address     => '0.0.0.0',
+  ssl_listen_address => '0.0.0.0',
+}
+class { 'puppetdb::master::config':
+  require => Class['puppetdb'],
+}
