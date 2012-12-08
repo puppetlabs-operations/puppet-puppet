24428358dfe4982546a694d5e5773ba3e51c1262
Joe Topjian <joe@topjian.net>
2012-12-08 06:05:43 +0000

    More boostrap changes

--- ext/bootstrap.sh
+++ ext/bootstrap.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y git puppet rubygems
 cd /etc/puppet/modules
-git clone https://github.com/jtopjian/puppet-puppet puppet
 git clone https://github.com/puppetlabs/puppetlabs-apt apt
 git clone -b 3.x https://github.com/puppetlabs/puppetlabs-stdlib stdlib
 git clone https://github.com/puppetlabs/puppetlabs-concat concat
-git clone https://github.com/puppetlabs/puppetlabs-ruby ruby
+git clone https://github.com/jtopjian/puppetlabs-ruby ruby
 git clone https://github.com/puppetlabs/puppetlabs-apache apache
 git clone https://github.com/puppetlabs/puppetlabs-passenger passenger
 git clone https://github.com/puppetlabs/puppetlabs-firewall firewall
@@ -19,3 +16,5 @@ for i in production development
 do
   mkdir -p env/$i/{modules,manifests}
 done
+
+echo "Now apply the pmaster.pp, and pmaster-puppetdb.pp manifests in that order"
