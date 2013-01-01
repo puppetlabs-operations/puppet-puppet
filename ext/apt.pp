a1a2f44ea77120026d6bf287f4766eea292d06f5
Joe Topjian <joe@topjian.net>
2013-01-01 20:54:37 +0000

    Removed ext files as they were site-specific

--- ext/apt.pp
+++
@@ -1,8 +0,0 @@
-apt::source { 'puppet':
-  location    => 'http://apt.terrarum.net/ubuntu',
-  release     => $::lsbdistcodename,
-  repos       => 'main',
-  key         => 'F8793AF4',
-  key_server  => 'subkeys.pgp.net',
-  include_src => false,
-}
