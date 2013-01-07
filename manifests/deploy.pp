b620f03da1bc9fcc0db53adf4bb24228edb4f1aa
Adrien Thebo <git@somethingsinistral.net>
2013-01-07 12:36:56 -0800

    Add ensure parameter for puppet::deploy

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -1,9 +1,9 @@
-class puppet::deploy($frequency = 6, $interval_in_minutes = 60) {
+class puppet::deploy($ensure = 'present', $frequency = 6, $interval_in_minutes = 60) {
 
   # Since 3aefec78893778f020759f947659e0f2bf30d776 we have
   # librarian-puppet support. See http://librarian-puppet.com/
   package { 'librarian-puppet':
-    ensure   => present,
+    ensure   => $ensure,
     provider => gem,
   }
 
@@ -16,18 +16,22 @@ class puppet::deploy($frequency = 6, $interval_in_minutes = 60) {
   }
 
   file { "/usr/local/bin/puppet_deploy.rb":
-    owner   => root,
-    group   => root,
-    mode    => 0750,
-    source  => "puppet:///modules/puppet/puppet_deploy.rb",
+    ensure => $ensure,
+    owner  => root,
+    group  => root,
+    mode   => 0750,
+    source => "puppet:///modules/puppet/puppet_deploy.rb",
   }
 
   cron { "Puppet: puppet_deploy.rb":
+    ensure  => $ensure,
     user    => root,
     command => '/usr/local/bin/puppet_deploy.rb 1>/dev/null 2>/dev/null',
     minute  => '*/20',
     require => File["/usr/local/bin/puppet_deploy.rb"];
   }
 
-  mcollective::plugin {'agent/deploy': has_ddl => true, module => 'puppet' }
+  if $ensure == 'present' {
+    mcollective::plugin {'agent/deploy': has_ddl => true, module => 'puppet' }
+  }
 }
