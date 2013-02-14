2ff4f0a7623c3dc84a1e1f1394cccde4532adc8d
Adrien Thebo <git@somethingsinistral.net>
2013-02-13 16:18:42 -0800

    Refactor confenv to use semantic_version
    This change uses the puppet_semantic_version facts to determine the
    commands used. This also adds support for puppet 3.1

--- lib/facter/confenv.rb
+++ lib/facter/confenv.rb
@@ -8,14 +8,16 @@ Facter.add("confenv") do
       # 2.7.12 (Puppet Enterprise 2.5.1)
       #
       # We scan for things looking like semantic version strings then grab the first one.
-      version = Facter.value('puppetversion').scan(/\d+\.\d+/).first.to_f
-      case version
-      when 3.0
+      case Facter.value('puppet_major_version')
+      when 3
         cmd = %{#{path} config print environment --run_mode agent}
-      when 2.7
-        cmd = %{#{path} config print environment --mode agent}
-      when 2.6
-        cmd = %{#{path} agent --configprint environment}
+      when 2
+        case Facter.value('puppet_minor_version')
+        when 7
+          cmd = %{#{path} config print environment --mode agent}
+        when 6
+          cmd = %{#{path} agent --configprint environment}
+        end
       end
 
       if output = Facter::Util::Resolution.exec(cmd)
