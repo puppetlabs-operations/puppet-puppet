ee9d4c651203aaa9a6d397afc9e459d56f1e176a
ttres <tacianotres@gmail.com>
2013-02-25 16:25:52 -0300

    Following code style conventions

--- manifests/server/backup.pp
+++ manifests/server/backup.pp
@@ -2,7 +2,7 @@ class puppet::server::backup {
 
   include puppet::params
 
-  if defined(Class["bacula"]) {
+  if defined(Class['bacula']) {
     bacula::job {
       "${fqdn}-puppetmaster":
         files    => [$puppet_confdir,$puppet_vardir],
