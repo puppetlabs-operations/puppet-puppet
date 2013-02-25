3ac099b68c263c5d7fcbdb26e88b743b4f7366ca
ttres <tacianotres@gmail.com>
2013-02-25 16:35:51 -0300

    Following code style conventions

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -1,7 +1,7 @@
 class puppet::deploy($ensure = 'present', $frequency = 6, $interval_in_minutes = 60) {
 
   if $ensure == 'present' {
-    notify { "puppet::deploy is deprecated; please see https://github.com/puppetlabs-operations/puppet-r10k": }
+    notify { 'puppet::deploy is deprecated; please see https://github.com/puppetlabs-operations/puppet-r10k': }
   }
 
   # Since 3aefec78893778f020759f947659e0f2bf30d776 we have
@@ -19,20 +19,20 @@ class puppet::deploy($ensure = 'present', $frequency = 6, $interval_in_minutes =
     before => Class['puppet::server'],
   }
 
-  file { "/usr/local/bin/puppet_deploy.rb":
+  file { '/usr/local/bin/puppet_deploy.rb':
     ensure => $ensure,
     owner  => root,
     group  => root,
     mode   => 0750,
-    source => "puppet:///modules/puppet/puppet_deploy.rb",
+    source => 'puppet:///modules/puppet/puppet_deploy.rb',
   }
 
-  cron { "Puppet: puppet_deploy.rb":
+  cron { 'Puppet: puppet_deploy.rb':
     ensure  => $ensure,
     user    => root,
     command => '/usr/local/bin/puppet_deploy.rb 1>/dev/null 2>/dev/null',
     minute  => '*/20',
-    require => File["/usr/local/bin/puppet_deploy.rb"];
+    require => File['/usr/local/bin/puppet_deploy.rb'];
   }
 
   if $ensure == 'present' {
