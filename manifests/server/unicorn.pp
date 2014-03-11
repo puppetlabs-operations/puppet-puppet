f71cd7f3629abc0b075e01f511444258fbf4000e
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-11 09:41:55 +0000

    Replace nginx::vhost with nginx::unicorn resource.
    Try to replace custom Nginx config template with parameters passed to
    nginx::unicorn. Use server::servername param or clientcert for Nginx
    vhost servername and SSL certificates.

--- manifests/server/unicorn.pp
+++ manifests/server/unicorn.pp
@@ -7,22 +7,37 @@ class puppet::server::unicorn {
   class { 'puppet::server::standalone':
     enabled => false,
     before  => [
-      Nginx::Vhost['puppetmaster'],
+      Nginx::Unicorn['puppetmaster'],
       Unicorn::App['puppetmaster'],
     ],
   }
 
-  nginx::vhost { "puppetmaster":
-    port     => 8140,
-    template => "puppet/vhost/nginx/unicorn.conf.erb",
+  $servername     = pick($::puppet::server::servername, $::clientcert)
+  $unicorn_socket = "${puppet::params::puppet_rundir}/puppetmaster_unicorn.sock"
+
+  nginx::unicorn { "puppetmaster":
+    servername        => $servername,
+    path              => $::puppet::params::puppet_confdir,
+    unicorn_socket    => $unicorn_socket,
+    ssl               => true,
+    sslonly           => true,
+    ssl_port          => '8140',
+    ssl_cert          => "${::puppet::ssldir}/certs/${servername}.pem",
+    ssl_key           => "${::puppet::ssldir}/private_keys/${servername}.pem",
+    ssl_ca            => "${::puppet::ssldir}/certs/ca.pem",
+    ssl_crl_path      => "${::puppet::ssldir}/crl.pem",
+    ssl_ciphers       => "EECDH+ECDSA+AESGCM:EECDH+aRSA+AESGCM:EECDH+ECDSA+SHA384:EECDH+ECDSA+SHA256:EECDH+aRSA+SHA384:EECDH+aRSA+SHA256:EECDH+aRSA+RC4:EECDH:EDH+aRSA:RC4:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS",
+    ssl_protocols     => "TLSv1.2 TLSv1.1 TLSv1 SSLv3",
+    ssl_verify_client => "optional",
+    magic             => "proxy_connect_timeout 300s;\n  proxy_read_timeout 300s;",
   }
 
   unicorn::app { "puppetmaster":
     approot         => $::puppet::params::puppet_confdir,
     config_file     => "${::puppet::params::puppet_confdir}/unicorn.conf",
-    pidfile         => "${puppet::params::puppet_rundir}/puppetmaster_unicorn.pid",
-    socket          => "${puppet::params::puppet_rundir}/puppetmaster_unicorn.sock",
-    logdir          => $puppet::params::puppet_logdir,
+    pidfile         => "${::puppet::params::puppet_rundir}/puppetmaster_unicorn.pid",
+    socket          => $unicorn_socket,
+    logdir          => $::puppet::params::puppet_logdir,
     user            => 'puppet',
     group           => 'puppet',
     before          => Service['nginx'],
