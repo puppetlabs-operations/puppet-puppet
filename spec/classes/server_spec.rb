4dd40a651444118b710a20c049a4b97f82ef6002
Daniel Dreier <ddreier@thinkplango.com>
2014-03-20 22:31:55 -0700

    Add mostly-passing rspec tests for passenger-based puppet servers

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -1,28 +1,92 @@
 require 'spec_helper'
 
 describe 'puppet::server' do
-  context 'supported operating systems' do
-    ['Debian', 'RedHat', 'CentOS'].each do |operatingsystem|
+  describe "webrick puppet::server" do
+    let(:params) {{
+      :servertype => 'standalone',
+      :manifest   => '/etc/puppet/manifests/site.pp',
+      :modulepath => ['/etc/puppet/environments/production/modules'],
+      :ca         => true,
+    }}
+    context 'CentOS/RedHat' do
+      ['RedHat', 'CentOS'].each do |operatingsystem|
+        let(:facts) {{ :operatingsystem => operatingsystem }}
+        it { should contain_class('puppet::server') }
+        it { should contain_class('puppet::server::standalone') }
+        it { should contain_class('puppet::package') }
+        it { should contain_class('puppet::agent') }
 
-      describe "basic webrick puppet::server on #{operatingsystem}" do
-        let(:params) {{
-          :servertype => 'standalone',
-          :manifest   => '/etc/puppet/manifests/site.pp',
-          :ca         => true,
-        }}
-        let(:facts) {{
-          :operatingsystem => operatingsystem,
-        }}
 
         it { should compile.with_all_deps }
-
         it { should contain_class('puppet::params') }
         it { should contain_class('puppet::server::config') }
+        it { should contain_ini_setting('modulepath').with_value('/etc/puppet/environments/production/modules') }
+        it { should contain_ini_setting('ca').with_value(true) }
 
-        it { should contain_ini_setting('modulepath') }
-        it do 
+        it { should contain_package('puppet-server') }
+        it do
           should contain_service('puppetmaster').with({
             :ensure => "running"
+          })
+        end
+      end
+    end
+    context 'Debian/Ubuntu' do
+      ['Debian','Ubuntu'].each do |operatingsystem|
+        let(:facts) {{ :operatingsystem => operatingsystem }}
+
+        it { should compile.with_all_deps }
+        it { should contain_class('puppet::params') }
+        it { should contain_class('puppet::server::config') }
+        it { should contain_ini_setting('modulepath').with_value('/etc/puppet/environments/production/modules') }
+        it { should contain_ini_setting('ca').with_value(true) }
+
+        it { should contain_package('puppetmaster') }
+
+        it do
+          should contain_service('puppetmaster').with({
+            :ensure => "running"
+          })
+        end
+      end
+    end
+  end
+  describe "passenger puppet::server" do
+    let(:params) {{
+      :servertype   => 'passenger',
+      :storeconfigs => 'puppetdb',
+      :manifest     => '/etc/puppet/manifests/site.pp',
+      :modulepath   => ['/etc/puppet/environments/production/modules'],
+      :ca           => true,
+    }}
+    context 'CentOS/RedHat' do
+      ['RedHat', 'CentOS'].each do |operatingsystem|
+        let(:facts) {{ 
+          :operatingsystem => operatingsystem,
+          :operatingsystemrelease => '6',
+          :osfamily => 'redhat',
+          :puppetversion => '3.4.2',
+          :concat_basedir => '/foo'
+        }}
+        it { should contain_class('puppet::server') }
+        it { should contain_class('puppet::package') }
+        it { should contain_class('puppet::agent') }
+        it { should contain_class('puppet::passenger') }
+
+
+        it { should compile.with_all_deps }
+        it { should contain_class('puppet::params') }
+        it { should contain_class('puppet::server::config') }
+        it { should contain_ini_setting('modulepath').with_value('/etc/puppet/environments/production/modules') }
+        it { should contain_ini_setting('ca').with_value(true) }
+
+        it { should contain_package('puppet-server') }
+        it do
+          should contain_service('httpd').with({
+            :ensure => "running"
+          })
+          should contain_service('puppetmaster').with({
+            :ensure => "stopped"
           })
         end
       end
