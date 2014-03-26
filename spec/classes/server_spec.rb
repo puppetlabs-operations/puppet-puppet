02bd3693292ce462ed9b1e9c3f2a1a073be960a2
Daniel Dreier <ddreier@thinkplango.com>
2014-03-25 18:19:25 -0700

    Refactor puppet::server spec tests to reduce code duplication

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
+shared_examples_for "all puppet master types" do
+  it { should compile.with_all_deps }
+  it { should contain_class('puppet::server') }
+  it { should contain_class('puppet::package') }
+  it { should contain_class('puppet::agent') }
+  it { should contain_class('puppet::params') }
+  it { should contain_class('puppet::server::config') }
+end
+
 describe 'puppet::server' do
   describe "webrick puppet::server" do
     let(:params) {{
@@ -8,45 +17,46 @@ describe 'puppet::server' do
       :modulepath => ['/etc/puppet/environments/production/modules'],
       :ca         => true,
     }}
-    context 'CentOS/RedHat' do
+    shared_examples_for "all standalone masters" do
+      it { should contain_ini_setting('modulepath').with_value('/etc/puppet/environments/production/modules') }
+      it { should contain_ini_setting('ca').with_value(true) }
+      it { should contain_class('puppet::server::standalone') }
+    end
+
+    context 'RedHat-derived Distros' do
       ['RedHat', 'CentOS'].each do |operatingsystem|
-        let(:facts) {{ :operatingsystem => operatingsystem }}
-        it { should contain_class('puppet::server') }
-        it { should contain_class('puppet::server::standalone') }
-        it { should contain_class('puppet::package') }
-        it { should contain_class('puppet::agent') }
+        context "#{operatingsystem}" do
+          let(:facts) {{ :operatingsystem => operatingsystem }}
 
+          it_behaves_like "all puppet master types"
+          it_behaves_like "all standalone masters"
 
-        it { should compile.with_all_deps }
-        it { should contain_class('puppet::params') }
-        it { should contain_class('puppet::server::config') }
-        it { should contain_ini_setting('modulepath').with_value('/etc/puppet/environments/production/modules') }
-        it { should contain_ini_setting('ca').with_value(true) }
-
-        it { should contain_package('puppet-server') }
-        it do
-          should contain_service('puppetmaster').with({
-            :ensure => "running"
-          })
+          # RHEL-specific examples
+          it { should contain_package('puppet-server') }
+          it do
+            should contain_service('puppetmaster').with({
+              :ensure => "running"
+            })
+          end
         end
       end
     end
-    context 'Debian/Ubuntu' do
+
+    context 'Debian-derived distros' do
       ['Debian','Ubuntu'].each do |operatingsystem|
-        let(:facts) {{ :operatingsystem => operatingsystem }}
+        context "#{operatingsystem}" do
+          let(:facts) {{ :operatingsystem => operatingsystem }}
 
-        it { should compile.with_all_deps }
-        it { should contain_class('puppet::params') }
-        it { should contain_class('puppet::server::config') }
-        it { should contain_ini_setting('modulepath').with_value('/etc/puppet/environments/production/modules') }
-        it { should contain_ini_setting('ca').with_value(true) }
+          it_behaves_like "all puppet master types"
+          it_behaves_like "all standalone masters"
 
-        it { should contain_package('puppetmaster') }
-
-        it do
-          should contain_service('puppetmaster').with({
-            :ensure => "running"
-          })
+          # Debian-specific examples
+          it { should contain_package('puppetmaster') }
+          it do
+            should contain_service('puppetmaster').with({
+              :ensure => "running"
+            })
+          end
         end
       end
     end
@@ -68,24 +78,18 @@ describe 'puppet::server' do
           :puppetversion => '3.4.2',
           :concat_basedir => '/foo'
         }}
-        # Tests common to all systems
-        it { should compile.with_all_deps }
+        it_behaves_like "all puppet master types"
 
-        # tests common to all puppet masters
-        it { should contain_class('puppet::server') }
-        it { should contain_class('puppet::package') }
-        it { should contain_class('puppet::agent') }
-        it { should contain_class('puppet::params') }
-        it { should contain_class('puppet::server::config') }
         it { should contain_ini_setting('modulepath').with_value('/etc/puppet/environments/production/modules') }
         it { should contain_ini_setting('ca').with_value(true) }
-        it { should contain_package('puppet-server') }
 
         # Tests specific to passenger server
         it { should contain_class('puppet::passenger') }
         it { should contain_class('apache') }
         it { should contain_class('apache::mod::passenger') }
 
+        # RHEL-family specific examples
+        it { should contain_package('puppet-server') }
         it do
           should contain_service('httpd').with({
             :ensure => "running"
@@ -114,15 +118,8 @@ describe 'puppet::server' do
         :concat_basedir => '/foo',
         :kernel         => 'linux',
       }}
-      # Tests common to all systems
-      it { should compile.with_all_deps }
+      it_behaves_like "all puppet master types"
 
-      # tests common to all puppet masters
-      it { should contain_class('puppet::server') }
-      it { should contain_class('puppet::package') }
-      it { should contain_class('puppet::agent') }
-      it { should contain_class('puppet::params') }
-      it { should contain_class('puppet::server::config') }
       it { should contain_ini_setting('modulepath').with_value('/etc/puppet/environments/production/modules') }
       it { should contain_ini_setting('ca').with_value(true) }
       it { should contain_package('puppetmaster') }
