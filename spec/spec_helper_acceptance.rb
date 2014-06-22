2192fcf15eac2a0adf4df3470a0ca07c55ed90f3
Daniel Dreier <ddreier@thinkplango.com>
2014-06-22 15:05:23 -0700

    Lock puppetlabs/ruby to 0.1.1 Current master branch of puppetlabs/ruby module breaks debian/ubuntu due to missing packages.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -38,7 +38,9 @@ masters.each do |host|
 mod 'stdlib',         :git => 'git://github.com/puppetlabs/puppetlabs-stdlib.git'
 mod 'apt',         :git => 'git://github.com/puppetlabs/puppetlabs-apt.git'
 mod 'concat',         :git => 'git://github.com/puppetlabs/puppetlabs-concat.git'
-mod 'ruby',           :git => 'git://github.com/puppetlabs/puppetlabs-ruby.git'
+mod 'ruby',
+  :git => 'git://github.com/puppetlabs/puppetlabs-ruby.git',
+  :ref => '0.1.1'
 mod 'puppetlabs_yum', :git => 'git://github.com/stahnma/puppet-module-puppetlabs_yum'
 mod 'puppetlabs_apt', :git => 'git://github.com/puppetlabs-operations/puppet-puppetlabs_apt.git'
 mod 'interval',       :git => 'git://github.com/puppetlabs-operations/puppet-interval.git'
