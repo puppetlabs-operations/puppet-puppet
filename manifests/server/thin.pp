2cbfefd142ec75c1b19ff663cabf673e876dc4b0
Daniel Dreier <ddreier@thinkplango.com>
2014-07-12 21:29:22 -0700

    Convert thin and unicorn to use jfryman/nginx This replaces use of the ploperations/nginx module with the jfryman/nginx module this does not fix the known issue of unicorn puppetmasters currently failing because of a missing HOME environment variable. That issue predates this commit.

--- manifests/server/thin.pp
+++ manifests/server/thin.pp
@@ -5,7 +5,7 @@ class puppet::server::thin {
 
   class { 'puppet::server::standalone': enabled => false }
   class { '::thin': }
-  class { 'nginx::server': }
+  class { 'nginx': }
 
   Ini_setting {
     ensure  => 'present',
@@ -22,10 +22,40 @@ class puppet::server::thin {
       setting => 'ssl_client_verify_header';
   }
 
-  $servers = $::processorcount
-  nginx::vhost { 'puppetmaster':
-    port     => 8140,
-    template => 'puppet/vhost/nginx/thin.conf.erb',
+  $servers     = $::processorcount
+  $servername  = pick($::puppet::server::servername, $::clientcert, $::fqdn)
+  $thin_socket = "unix:${puppet::params::puppet_rundir}/puppetmaster.0.sock"
+
+  nginx::resource::vhost { 'puppetmaster':
+    server_name        => [$servername],
+    ssl                => true,
+    ssl_port           => '8140',
+    listen_port        => '8140', # force ssl_only by matching ssl_port
+    ssl_cert           => "${::puppet::ssldir}/certs/${servername}.pem",
+    ssl_key            => "${::puppet::ssldir}/private_keys/${servername}.pem",
+    ssl_ciphers        => $::puppet::server::ssl_ciphers,
+    ssl_protocols      => $::puppet::server::ssl_protocols,
+    proxy_read_timeout => '300',
+    proxy              => 'http://puppetmaster_thin',
+    vhost_cfg_append   => {
+      ssl_crl                => "${::puppet::ssldir}/crl.pem",
+      ssl_client_certificate => "${::puppet::ssldir}/certs/ca.pem",
+      ssl_verify_client      => 'optional',
+      proxy_connect_timeout  => '300',
+      proxy_set_header       => [ 'Host $host',
+                                'X-Real-IP $remote_addr',
+                                'X-Forwarded-For $proxy_add_x_forwarded_for',
+                                'X-Client-Verify $ssl_client_verify',
+                                'X-Client-DN $ssl_client_s_dn',
+                                'X-SSL-Issuer $ssl_client_i_dn'],
+      root                   => '/usr/share/empty',
+    }
+  }
+
+  nginx::resource::upstream { 'puppetmaster_thin':
+    members => [
+      $thin_socket
+    ],
   }
 
   concat::fragment { 'proctitle':
@@ -41,7 +71,7 @@ class puppet::server::thin {
     chdir      => $puppet::params::puppet_confdir,
     subscribe  => Concat["${::puppet::params::puppet_confdir}/config.ru"],
     require    => Class['::thin'],
-    socket     => '/var/run/thin/puppetmaster.sock',
+    socket     => "${puppet::params::puppet_rundir}/puppetmaster.sock",
     force_home => '/etc/puppet',
   }
 }
