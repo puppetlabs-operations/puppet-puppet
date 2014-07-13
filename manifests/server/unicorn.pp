1fa2d1c6ff825a9e201a9bbdad358ce12c9ee19b
Daniel Dreier <ddreier@thinkplango.com>
2014-07-12 21:29:22 -0700

    Convert thin and unicorn to use jfryman/nginx This replaces use of the ploperations/nginx module with the jfryman/nginx module this does not fix the known issue of unicorn puppetmasters currently failing because of a missing HOME environment variable. That issue predates this commit.

--- manifests/server/unicorn.pp
+++ manifests/server/unicorn.pp
@@ -2,45 +2,61 @@ class puppet::server::unicorn {
 
   include puppet::params
   include puppet::server::rack
-  include nginx::server
+  class { 'nginx': }
+
+  file { '/etc/puppet':
+    ensure => 'directory',
+    owner  => 'puppet',
+    group  => 'puppet',
+  }
 
   class { 'puppet::server::standalone':
     enabled => false,
     before  => [
-      Nginx::Unicorn['puppetmaster'],
+      Nginx::Resource::Vhost['puppetmaster'],
       Unicorn::App['puppetmaster'],
     ],
   }
 
   $servername     = pick($::puppet::server::servername, $::clientcert, $::fqdn)
-  $unicorn_socket = "${puppet::params::puppet_rundir}/puppetmaster_unicorn.sock"
+  $unicorn_socket = "unix:${puppet::params::puppet_rundir}/puppetmaster_unicorn.sock"
 
-  nginx::unicorn { 'puppetmaster':
-    servername        => $servername,
-    path              => $::puppet::params::puppet_confdir,
-    unicorn_socket    => $unicorn_socket,
-    ssl               => true,
-    sslonly           => true,
-    ssl_port          => '8140',
-    ssl_cert          => "${::puppet::ssldir}/certs/${servername}.pem",
-    ssl_key           => "${::puppet::ssldir}/private_keys/${servername}.pem",
-    ssl_ca            => "${::puppet::ssldir}/certs/ca.pem",
-    ssl_crl_path      => "${::puppet::ssldir}/crl.pem",
-    ssl_ciphers       => $::puppet::server::ssl_ciphers,
-    ssl_protocols     => $::puppet::server::ssl_protocols,
-    ssl_verify_client => 'optional',
-    magic             => "proxy_connect_timeout 300s;\n  proxy_read_timeout 300s;",
+  nginx::resource::vhost { 'puppetmaster':
+    server_name           => [$servername],
+    ssl                   => true,
+    ssl_port              => '8140',
+    listen_port           => '8140', # force ssl_only by matching ssl_port
+    ssl_cert              => "${::puppet::ssldir}/certs/${servername}.pem",
+    ssl_key               => "${::puppet::ssldir}/private_keys/${servername}.pem",
+    ssl_ciphers           => $::puppet::server::ssl_ciphers,
+    ssl_protocols         => $::puppet::server::ssl_protocols,
+    proxy_read_timeout    => '300',
+    proxy                 => "http://puppetmaster_unicorn",
+    vhost_cfg_append      => {
+      ssl_crl                => "${::puppet::ssldir}/crl.pem",
+      ssl_client_certificate => "${::puppet::ssldir}/certs/ca.pem",
+      ssl_verify_client      => 'optional',
+      proxy_connect_timeout  => '300',
+      proxy_set_header       => [ 'Host $host', 'X-Real-IP $remote_addr', 'X-Forwarded-For $proxy_add_x_forwarded_for', 'X-Client-Verify $ssl_client_verify', 'X-Client-DN $ssl_client_s_dn', 'X-SSL-Issuer $ssl_client_i_dn'],
+      root                   => '/usr/share/empty',
+    }
+  }
+
+  nginx::resource::upstream { 'puppetmaster_unicorn':
+    members => [
+      $unicorn_socket
+    ],
   }
 
   unicorn::app { 'puppetmaster':
-    approot         => $::puppet::params::puppet_confdir,
-    config_file     => "${::puppet::params::puppet_confdir}/unicorn.conf",
-    pidfile         => "${::puppet::params::puppet_rundir}/puppetmaster_unicorn.pid",
-    socket          => $unicorn_socket,
-    logdir          => $::puppet::params::puppet_logdir,
-    user            => 'puppet',
-    group           => 'puppet',
-    before          => Service['nginx'],
-    subscribe       => Concat["${::puppet::params::puppet_confdir}/config.ru"],
+    approot     => $::puppet::params::puppet_confdir,
+    config_file => "${::puppet::params::puppet_confdir}/unicorn.conf",
+    pidfile     => "${::puppet::params::puppet_rundir}/puppetmaster_unicorn.pid",
+    socket      => $unicorn_socket,
+    logdir      => $::puppet::params::puppet_logdir,
+    user        => 'puppet',
+    group       => 'puppet',
+    before      => Service['nginx'],
+#    export_home => $::confdir, # uncomment pending https://github.com/puppetlabs-operations/puppet-unicorn/pull/14
   }
 }
