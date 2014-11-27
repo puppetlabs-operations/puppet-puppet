908b497581b171955a7f4afef2870081c1c3bff7
Daniel Dreier <d@puppetlabs.com>
2014-11-26 22:09:35 -0800

    Trivial lint fixes

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -129,7 +129,7 @@ class puppet::server::config {
         value   =>  $puppet::server::dns_alt_names
     }
   }
-  
+
   if $puppet::server::autosign {
     ini_setting { 'autosign':
         setting => 'autosign',
