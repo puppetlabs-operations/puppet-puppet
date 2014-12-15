24727b6fab52cc0c0f32e7347f894c84e0dbdfd2
Daniel Dreier <d@puppetlabs.com>
2014-12-14 16:46:27 -0800

    Refactor rspec-puppet tests

--- spec/classes/puppet_spec.rb
+++ spec/classes/puppet_spec.rb
@@ -1,28 +1,31 @@
 require 'spec_helper'
 
-describe 'puppet' do
-  context 'supported operating systems' do
-    ['Debian', 'RedHat', 'CentOS'].each do |operatingsystem|
-      describe "puppet class without any parameters on #{operatingsystem}" do
-        let(:params) {{ }}
-        let(:facts) {{
-          :operatingsystem => operatingsystem,
-        }}
+PuppetSpecFacts.facts_for_platform_by_name([
+  "Debian_wheezy_7.7_amd64_3.7.2_structured",
+  "Ubuntu_precise_12.04_amd64_PE-3.3.2_stringified",
+  "Ubuntu_trusty_14.04_amd64_PE-3.3.2_stringified"]).each do |name, facthash|
 
-        it { should compile.with_all_deps }
+  describe 'puppet' do
+    context 'supported operating systems' do
+      ['Debian', 'RedHat', 'CentOS'].each do |operatingsystem|
+        describe "puppet class without any parameters on #{operatingsystem}" do
+          let(:params) {{ }}
+          let(:facts) { facthash }
 
-        it { should contain_class('puppet::params') }
+          it { should compile.with_all_deps }
+          it { should contain_class('puppet::params') }
+        end
       end
     end
-  end
 
-  context 'unsupported operating system' do
-    describe 'puppet class without any parameters on OpenVMS' do
-      let(:facts) {{
-        :operatingsystem => 'OpenVMS',
-      }}
+    context 'unsupported operating system' do
+      describe 'puppet class without any parameters on OpenVMS' do
+        let(:facts) {{
+          :operatingsystem => 'OpenVMS',
+        }}
 
-      it { expect { should contain_package('puppet') }.to raise_error(Puppet::Error, /Sorry, OpenVMS is not supported/) }
+        it { expect { should contain_package('puppet') }.to raise_error(Puppet::Error, /Sorry, OpenVMS is not supported/) }
+      end
     end
   end
 end
--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -20,183 +20,66 @@ shared_examples_for "basic puppetmaster environment config" do
   it { should contain_ini_setting('modulepath').with_ensure('absent') }
 end
 
-describe 'puppet::server' do
-  describe "webrick puppet::server" do
-    let(:params) {{
-      :servertype      => 'standalone',
-      :manifest        => '/etc/puppet/manifests/site.pp',
-      :modulepath      => ['/etc/puppet/environments/production/modules'],
-      :environmentpath => '$confdir/environments',
-      :ca              => true,
-    }}
-    shared_examples_for "all standalone masters" do
-      it { should contain_class('puppet::server::standalone') }
-    end
+PuppetSpecFacts.facts_for_platform_by_name(["Debian_wheezy_7.7_amd64_3.7.2_structured", "Ubuntu_precise_12.04_amd64_PE-3.3.2_stringified", "Ubuntu_trusty_14.04_amd64_PE-3.3.2_stringified"]).each do |name, facthash|
+describe "puppet::server" do
+    let(:facts) { facthash }
 
-    context 'RedHat-derived Distros' do
-      ['RedHat', 'CentOS'].each do |operatingsystem|
-        context "#{operatingsystem}" do
-          let(:facts) {{
-            :osfamily        => 'redhat',
-            :operatingsystem => operatingsystem,
+    context "running on #{name}" do
+      ['standalone','passenger','unicorn','thin'].each do |server_type|
+#        manage_repos = false if facthash['osfamily'] == 'FreeBSD'
+        context "servertype => #{server_type}" do
+          let(:params) {{
+            :servertype   => server_type,
+            :storeconfigs => 'puppetdb',
+            :manifest     => '/etc/puppet/manifests/site.pp',
+            :modulepath   => ['/etc/puppet/environments/production/modules'],
+            :environmentpath => '$confdir/environments',
+            :ca           => true,
           }}
 
+          case facthash['osfamily']
+          when 'RedHat'
+            puppetmaster_package = 'puppet-server'
+          when 'Debian'
+            puppetmaster_package = 'puppetmaster'
+
+          end
           it_behaves_like "all puppet master types"
-          it_behaves_like "all standalone masters"
           it_behaves_like "basic puppetmaster environment config"
 
-          # RHEL-specific examples
-          it { should contain_package('puppet-server') }
-          it do
-            should contain_service('puppetmaster').with({
-              :ensure => "running"
-            })
+          #it_behaves_like "agent examples"
+          case server_type
+            when 'standalone'
+            it {
+              should contain_class('puppet::server::standalone')
+              should contain_service('puppetmaster').with({ :ensure => "running" })
+              should contain_package(puppetmaster_package)
+            }
+
+            when 'passenger'
+              it {
+                should contain_class('puppet::server::passenger')
+                should contain_class('apache')
+                should contain_class('apache::mod::passenger')
+              }
+
+            when 'unicorn'
+              it {
+                should contain_class('puppet::server::unicorn')
+                should contain_service('puppetmaster').with({:ensure => "stopped"})
+                should contain_service('nginx').with({:ensure => "running"})
+                should contain_service('unicorn_puppetmaster').with({:ensure => "running"})
+              }
+            when 'thin'
+              it {
+                should contain_class('puppet::server::thin')
+                should contain_service('puppetmaster').with({ :ensure => "stopped" })
+                should contain_service('nginx').with({:ensure => "running"})
+                should contain_service('thin-puppetmaster').with({:ensure => "running"})
+                should contain_file('/etc/thin.d/puppetmaster.yml')
+              }
           end
         end
-      end
-    end
-
-    context 'Debian-derived distros' do
-      ['Debian','Ubuntu'].each do |operatingsystem|
-        context "#{operatingsystem}" do
-          let(:facts) {{
-            :operatingsystem => operatingsystem,
-            :osfamily        => 'debian',
-            :lsbdistid       => operatingsystem,
-            :lsbdistcodename => 'lolwut',
-          }}
-
-          it_behaves_like "all puppet master types"
-          it_behaves_like "all standalone masters"
-          it_behaves_like "basic puppetmaster environment config"
-
-          # Debian-specific examples
-          it { should contain_package('puppetmaster') }
-          it do
-            should contain_service('puppetmaster').with({
-              :ensure => "running"
-            })
-          end
-        end
-      end
-    end
-  end
-  describe "passenger puppet::server" do
-    let(:params) {{
-      :servertype   => 'passenger',
-      :storeconfigs => 'puppetdb',
-      :manifest     => '/etc/puppet/manifests/site.pp',
-      :modulepath   => ['/etc/puppet/environments/production/modules'],
-      :ca           => true,
-    }}
-    context 'CentOS/RedHat' do
-      ['RedHat', 'CentOS'].each do |operatingsystem|
-        let(:facts) {{
-          :operatingsystem => operatingsystem,
-          :operatingsystemrelease => '6',
-          :osfamily => 'redhat',
-          :puppetversion => '3.4.2',
-          :concat_basedir => '/foo'
-        }}
-        it_behaves_like "all puppet master types"
-        it_behaves_like "basic puppetmaster config"
-
-        # Tests specific to passenger server
-        it { should contain_class('puppet::server::passenger') }
-        it { should contain_class('apache') }
-        it { should contain_class('apache::mod::passenger') }
-
-        # RHEL-family specific examples
-        it { should contain_package('puppet-server') }
-        it do
-          should contain_service('httpd').with({
-            :ensure => "running"
-          })
-          should contain_service('puppetmaster').with({
-            :ensure => "stopped"
-          })
-        end
-      end
-    end
-  end
-  describe "unicorn puppet::server" do
-    let(:params) {{
-      :servertype   => 'unicorn',
-      :storeconfigs => 'puppetdb',
-      :manifest     => '/etc/puppet/manifests/site.pp',
-      :modulepath   => ['/etc/puppet/environments/production/modules'],
-      :ca           => true,
-    }}
-    context 'Debian' do
-      let(:facts) {{
-        :operatingsystem        => 'debian',
-        :operatingsystemrelease => '7',
-        :osfamily               => 'debian',
-        :puppetversion          => '3.4.2',
-        :concat_basedir         => '/foo',
-        :kernel                 => 'linux',
-        :lsbdistid              => 'debian',
-        :lsbdistcodename        => 'wheezy',
-      }}
-      it_behaves_like "all puppet master types"
-      it_behaves_like "basic puppetmaster config"
-
-      it { should contain_package('puppetmaster') }
-
-      # Tests specific to passenger server
-      it { should contain_class('puppet::server::unicorn') }
-
-      it do
-        should contain_service('puppetmaster').with({
-          :ensure => "stopped"
-        })
-        should contain_service('nginx').with({
-          :ensure => "running"
-        })
-        should contain_service('unicorn_puppetmaster').with({
-          :ensure => "running"
-        })
-      end
-    end
-  end
-  describe "thin puppet::server" do
-    let(:params) {{
-      :servertype   => 'thin',
-      :storeconfigs => 'puppetdb',
-      :manifest     => '/etc/puppet/manifests/site.pp',
-      :modulepath   => ['/etc/puppet/environments/production/modules'],
-      :ca           => true,
-    }}
-    context 'Debian' do
-      let(:facts) {{
-        :operatingsystem        => 'debian',
-        :operatingsystemrelease => '7',
-        :osfamily               => 'debian',
-        :puppetversion          => '3.4.2',
-        :concat_basedir         => '/foo',
-        :kernel                 => 'linux',
-        :lsbdistid              => 'debian',
-        :lsbdistcodename        => 'wheezy',
-      }}
-      it_behaves_like "all puppet master types"
-      it_behaves_like "basic puppetmaster config"
-
-      it { should contain_package('puppetmaster') }
-
-      # Tests specific to passenger server
-      it { should contain_class('puppet::server::thin') }
-
-      it do
-        should contain_service('puppetmaster').with({
-          :ensure => "stopped"
-        })
-        should contain_service('nginx').with({
-          :ensure => "running"
-        })
-        should contain_service('thin-puppetmaster').with({
-          :ensure => "running"
-        })
-        should contain_file('/etc/thin.d/puppetmaster.yml')
       end
     end
   end
