a1705e02fc32e501f49d44fc1fc4e25c51e50987
Zach Leslie <zach@puppetlabs.com>
2015-02-10 20:34:37 -0800

    Add support for SRV records
    This extends the puppet::config class to include two new parameters that
    will allow the user to enable or disable SRV record support for master
    discovery.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,14 +1,18 @@
 class puppet::config (
-  $logdir  = $puppet::logdir,
-  $vardir  = $puppet::vardir,
-  $ssldir  = $puppet::ssldir,
-  $rundir  = $puppet::rundir,
-  $confdir = $puppet::confdir,
-  $user    = $puppet::user,
-  $group   = $puppet::group,
-  $conf    = $puppet::conf,
+  $logdir          = $puppet::logdir,
+  $vardir          = $puppet::vardir,
+  $ssldir          = $puppet::ssldir,
+  $rundir          = $puppet::rundir,
+  $confdir         = $puppet::confdir,
+  $user            = $puppet::user,
+  $group           = $puppet::group,
+  $conf            = $puppet::conf,
+  $use_srv_records = false,
+  $srv_domain      = $::domain,
 ) inherits puppet {
   include puppet::params
+
+  validate_bool($use_srv_records)
 
   Ini_setting {
     path    => $conf,
@@ -34,5 +38,29 @@ class puppet::config (
   ini_setting { 'rundir':
     setting => 'rundir',
     value   => $rundir,
+  }
+
+  if $use_srv_records == true {
+    ini_setting { 'use_srv_records':
+      setting => 'use_srv_records',
+      value   => $use_srv_records,
+    }
+
+    ini_setting { 'srv_domain':
+      setting => 'srv_domain',
+      value   => $srv_domain,
+    }
+  } else {
+    ini_setting { 'use_srv_records':
+      ensure  => 'absent',
+      setting => 'use_srv_records',
+      value   => $use_srv_records,
+    }
+
+    ini_setting { 'srv_domain':
+      ensure  => 'absent',
+      setting => 'srv_domain',
+      value   => $srv_domain,
+    }
   }
 }
